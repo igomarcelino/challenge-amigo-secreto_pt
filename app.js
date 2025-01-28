e0198abf3570579c26cccbe629e7a575278cca33
Igo Marcelino <geslucian@hotmail.com>
2025-01-28 16:19:12 -0300

    feat: Adiciona a funcao para adicionar amigos

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+let nome_amigo = document.getElementById('amigo');
+function adicionarAmigo(){
+    amigos.push(nome_amigo.value);
+    console.log(amigos);
+    nome_amigo.value = "";
+}
+
+
